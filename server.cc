262157f8a91bc826fac31fe0fadf5eb5f6b64955
Qingqing Li <liqingqing@tamu.edu>
2017-09-19 15:05:42 -0500

    populated registerClient on server

--- server.cc
+++ server.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <chrono>
 #include <memory>
+#include <pthread.h>
 
 #include <grpc/grpc.h>
 #include <grpc++/server.h>
@@ -27,16 +28,18 @@ using std::chrono::system_clock;
 
 using namespace std;
 
+void* RunRoom(void* param);
+
 class MainServerImpl final : public MainServer::Service {
 	public:
 
 		MainServerImpl(){;}
 		
 		Status RegisterClient(ServerContext* context, const Request* request,
-												ChatRoom* response) override{;}
+												ChatRoom* response) override;
 		
 		Status ListRoom(ServerContext* context, const Request* request,
-										ServerWriter<ChatRoom>* response) override{;}
+										ServerWriter<ChatRoom>* response) override;
 		Status JoinRoom(ServerContext* context, const Request* request,
 										Request* response) override{;}
 		Status LeaveRoom(ServerContext* context, const Request* request,
@@ -74,8 +77,87 @@ void RunServer(){
 	server->Wait();
 }
 
+Status
+MainServerImpl::
+RegisterClient(ServerContext* context,
+							 const Request* request,
+								ChatRoom* response) {
+	// identify the client name from request	
+	// update client database
+	// create a chat room thread owned by client
+	// update chatroom database
+	// run the chat room server
+	
+	string who = request->from();
+	unique_ptr<Client> c;
+	c->set_name(who);
+		
+	unique_ptr<ChatRoom> cr(c->add_chatroom());
+	// TO-DO : find next available port
+	int port;
+	pthread_t thread;
+	pthread_create(&thread, NULL, RunRoom, (void*)&port);
+	cr->set_owner(who);
+	cr->set_thread(thread);
+	cr->set_port(port);
+	
+	// cr->add_clients();
+	
+	this->clients.insert(make_pair(who, c));
+	this->chatRooms.insert(make_pair(who, cr));
+	
+	response = cr;
+}
 
+Status
+MainServerImpl::
+ListRoom(ServerContext* context, 
+					const Request* request,
+					ServerWriter<ChatRoom>* response) {
+	// traverse the chat room databases
+	// identify the chat room owned by querying client
+}
 
+Status
+MainServerImpl::
+JoinRoom(ServerContext* context,
+				 const Request* request,
+					Request* response) {
+	// add client to chat room list
+}
+
+Status
+MainServerImpl::
+LeaveRoom(ServerContext* context,
+					const Request* request,
+					Request* response) {
+										
+}
+
+Status
+MainServerImpl::
+Chat(ServerContext* context,
+		 const Request* request,
+			Request* response) {
+								
+}
+
+/*------------------------------------Chat Room Server------------------------------------------*/
+
+void* RunRoom(void* param) {
+	// TO-DO: find next available port
+	int* o = (int*)param;
+	int port = *o;
+	string server_address("localhost:50051");
+	RoomServerImpl service;
+	
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Chat Room listening on " << server_address << std::endl;
+	server->Wait();
+}
 
 int main(int argc, char** argv){
 	RunServer();
